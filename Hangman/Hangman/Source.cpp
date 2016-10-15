3ee42e1806c642c7e41e123fcbf5f9cddfc34fcf
Will Velida <willvelida@hotmail.co.uk>
2016-10-15 18:44:46 +1300

    Adding Hangman game to repo

---
+++ Hangman/Hangman/Source.cpp
@@ -0,0 +1,85 @@
+// Hangman
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+	// Setup variables and constants
+	const int MAX_WRONG = 8; // maximum number of incorrect guesses allowed
+
+	vector<string> words; // collection of possible words to guess
+	words.push_back("GUESS");
+	words.push_back("HANGMAN");
+	words.push_back("DIFFICULT");
+
+	srand(static_cast<unsigned int>(time(0)));
+	random_shuffle(words.begin(), words.end());
+	const string THE_WORD = words[0];
+	int wrong = 0;
+	string soFar(THE_WORD.size(), '-');
+	string used = "";
+
+	cout << "Welcome to Hangman!\n";
+
+	// The main game loop
+	while ((wrong < MAX_WRONG) && (soFar != THE_WORD))
+	{
+		cout << "\n\nYou have " << (MAX_WRONG - wrong);
+		cout << " incorrect guesses left.\n";
+		cout << "\nYou've used the following letters:\n" << used << endl;
+		cout << "\nSo far, the word is:\n" << soFar << endl;
+
+		char guess;
+		cout << "\n\nEnter your guess: ";
+		cin >> guess;
+		guess = toupper(guess); // Make uppercase since our word is uppercase
+		while (used.find(guess) != string::npos)
+		{
+			cout << "\nYou've already guessed " << guess << endl;
+			cout << "Enter your guess: ";
+			cin >> guess;
+			guess = toupper(guess);
+		}
+
+		used += guess;
+
+		if (THE_WORD.find(guess) != string::npos)
+		{
+			cout << "That's right! " << guess << " is in the word.\n";
+
+			// update soFar to include newly guessed letter
+			for (int i = 0; i < THE_WORD.length(); i++)
+			{
+				if (THE_WORD[i] == guess)
+				{
+					soFar[i] = guess;
+				}
+			}
+		}
+		else
+		{
+			cout << "Sorry, " << guess << " isn't in the word.\n";
+			wrong++;
+		}
+
+		// Ending the game
+		if (wrong == MAX_WRONG)
+		{
+			cout << "\n You Lose!!";
+		}
+		else
+		{
+			cout << "\n You Win!!";
+		}
+	}
+	
+	cout << "\nThe word was " << THE_WORD << endl;
+	return 0;
+}
